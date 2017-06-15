91bf89144be170bbf3dd9df12ae95b9387163a30
Brice Waegeneire <brice.wge@gmail.com>
2017-06-15 17:42:06 +0200

    ex06 made the logic more straight forward

--- ex06/members_only.php
+++ ex06/members_only.php
@@ -1,14 +1,14 @@
 <?PHP
 
-if ($_SERVER['PHP_AUTH_USER'] !== "zaz"
-    and $_SERVER['PHP_AUTH_PW'] !== "jaimelespetitsponeys") {
+if ($_SERVER['PHP_AUTH_USER'] === "zaz"
+    and $_SERVER['PHP_AUTH_PW'] === "jaimelespetitsponeys") {
+    $image = file_get_contents("../img/42.png");
+    $image = base64_encode($image);
+    echo "<html><body>Hello Zaz<br />\n<img src='data:image/png;base64,$image'>\n</body></html>\n";
+} else {
     header("WWW-Authenticate: Basic realm=''Member area''");
     header('HTTP/1.0 401 Unauthorized');
     echo "<html><body>That area is accessible for members only</body></html>\n";
-} else {
-    $image = file_get_contents("../img/42.png");
-    $image = base64_encode($image);
-    echo "<html><body>\nHello Zaz<br />\n<img src='data:image/png;base64,$image'>\n</body></html>\n";
 }
 
 ?>
