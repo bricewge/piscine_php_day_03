f27426a11ee611c93aa8a7135a30b67d38919b3a
Brice Waegeneire <brice.wge@gmail.com>
2017-06-16 10:49:25 +0200

    ex06 minor fix

--- ex06/members_only.php
+++ ex06/members_only.php
@@ -4,7 +4,7 @@ if ($_SERVER['PHP_AUTH_USER'] === "zaz"
     and $_SERVER['PHP_AUTH_PW'] === "jaimelespetitsponeys") {
     $image = file_get_contents("../img/42.png");
     $image = base64_encode($image);
-    echo "<html><body>Hello Zaz<br />\n<img src='data:image/png;base64,$image'>\n</body></html>\n";
+    echo "<html><body>\nHello Zaz<br />\n<img src='data:image/png;base64,$image'>\n</body></html>\n";
 } else {
     header("WWW-Authenticate: Basic realm=''Member area''");
     header('HTTP/1.0 401 Unauthorized');
